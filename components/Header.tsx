0722a676e916aa7a6883da6b5ec4df5957c04c11
Mykhailo Diachuk <dyachukmihail@gmail.com>
2022-08-15 21:31:07 +0300

    :bug: fixed header zIndex

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import DarkModeSwitch from './DarkModeSwitch'
 
 export const Header = () => {
   return (
-    <div className="absolute px-5 lg:px-10 py-5 w-full top-0 left-0 flex justify-between items-center">
+    <div className="absolute px-5 lg:px-10 py-5 w-full top-0 left-0 flex justify-between items-center z-10">
       <span className="font-sans text-3xl font-bold text-black dark:text-white relative drop-shadow-text dark:drop-shadow-textDark">
         1337
       </span>
